4882265ca0520957f43a355547131fb916cb2e3e
W0467817 <brett.longpre@outlook.com>
2023-02-05 21:46:33 -0400

    Final

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,62 @@
 public class Animal {
+
+    private String species;
+    private double maxWeight;
+    private String environment;
+    private boolean endangered;
+
+
+    // Default constructor
+    public Animal() {
+        this.endangered = false;
+    }
+
+    // Overloaded constructor
+    public Animal(String species, double maxWeight, String environment, boolean endangered) {
+        this.species = species;
+        this. maxWeight = maxWeight;
+        this.environment = environment;
+        this.endangered = endangered;
+    }
+
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public double getMaxWeight() {
+        return maxWeight;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public boolean endangered() {
+        return endangered;
+    }
+
+
+    // Feed the animal resulting in an increase in weight
+    public String feedMe(String foodType) {
+        this.maxWeight++;
+        return "You fed the " + this.species + " a " + foodType;
+    }
+
+    // The animal makes a sound
+    public String makeASound(String sound) {
+        return "The " + this.species + " goes " + sound;
+
+    }
+
+    // Check if animal is endangered
+    public String toString() {
+        String output = "I am a " + this.maxWeight + " lb " + this.species + " that lives in the " + this.environment + ". I ";
+        String am = "am";
+        if (!this.endangered) {
+            am += " not ";
+        }
+        output += am + " an endangered species";
+        return output;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Animal rat = new Animal("rat", 1, "sewers", false);
+        Animal elephant = new Animal("Elephant", 2000, "Savannah", true);
+        Animal fish = new Animal("fish", 50, "ocean", false);
+
+
+        // Object array
+        Animal[] zoo = new Animal[] {elephant, rat, fish};
+
+
+        for (int i = 0; i < zoo.length; i++) {
+            System.out.println("\n" + zoo[i].toString());
+            if (zoo[i] == rat) {
+                System.out.println(zoo[i].makeASound("woof"));
+            } else if (zoo[i] == elephant) {
+                System.out.println(zoo[i].feedMe("peanut"));
+            } else if (zoo[i] == fish) {
+                System.out.println(zoo[i].makeASound("glub-glub"));
+            }
+        }
     }
 }
